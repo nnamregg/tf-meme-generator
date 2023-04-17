aa023f4b17e0f3753dcc37767b2186a71b1a5dbf
German Troncoso <nnamreg.t@gmail.com>
2023-04-16 22:47:52 -0300

    Implemento html2canvas y agrego componente de modal. Agrego assets (favicon y fuente)

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from "react";
+import html2canvas from "html2canvas";
+import Modal from "./Modal";
 
 function Meme() {
     const [meme, setMeme] = useState({
@@ -7,6 +9,7 @@ function Meme() {
         randomImage: "http://i.imgflip.com/1bij.jpg" 
     })
     const [allMemes, setAllMemes] = useState([])
+    const [showModal, setShowModal] = useState(false)
 
     useEffect(() => {
         async function getMemes() {
@@ -35,38 +38,73 @@ function Meme() {
         }))
     }
 
+    function makeCaptureCanvas() {
+        const meme = document.getElementById('memeCapture');
+        const container = document.createElement('div');
+        container.className = 'save-modal--container';
+        container.id = 'memeExport';
+
+        html2canvas(meme, { allowTaint: true }).then(function(canvas) {
+            canvas.id = 'memeCanvas';
+            container.appendChild(canvas);
+        });
+
+        return container;
+    }
+
+    const handleModal = () => {
+        setShowModal(!showModal)
+    }
+
     return (
-        <main>
-            <div className="form">
-                <input 
-                    type="text"
-                    placeholder="Top text"
-                    className="form--input"
-                    name="topText"
-                    value={meme.topText}
-                    onChange={handleChange}
-                />
-                <input 
-                    type="text"
-                    placeholder="Bottom text"
-                    className="form--input"
-                    name="bottomText"
-                    value={meme.bottomText}
-                    onChange={handleChange}
-                />
-                <button 
-                    className="form--button"
-                    onClick={getMemeImage}
-                >
-                    Cambiar imagen 🖼
-                </button>
-            </div>
-            <div className="meme">
-                <img src={meme.randomImage} className="meme--image" />
-                <h2 className="meme--text top">{meme.topText}</h2>
-                <h2 className="meme--text bottom">{meme.bottomText}</h2>
-            </div>
-        </main>
+        <>
+            <main>
+                <div className="container">
+                    <div className="form">
+                        <input 
+                            type="text"
+                            placeholder="Top text"
+                            className="form--input"
+                            name="topText"
+                            value={meme.topText}
+                            onChange={handleChange}
+                        />
+                        <input 
+                            type="text"
+                            placeholder="Bottom text"
+                            className="form--input"
+                            name="bottomText"
+                            value={meme.bottomText}
+                            onChange={handleChange}
+                        />
+                        <button 
+                            className="form--button"
+                            onClick={getMemeImage}
+                        >
+                            Cambiar imagen 🖼
+                        </button>
+                    </div>
+                    <div className="meme" id="memeCapture">
+                        <img src={meme.randomImage} className="meme--image" />
+                        <h2 className="meme--text top">{meme.topText}</h2>
+                        <h2 className="meme--text bottom">{meme.bottomText}</h2>
+                    </div>
+                    <button 
+                        className="save--button"
+                        onClick={handleModal}
+                    >
+                        Guardar meme 💾
+                    </button>
+                </div>
+            </main>
+            { showModal ?
+                            <Modal canvas={makeCaptureCanvas()} handleModal={handleModal} />
+                        :
+                            null
+            }
+        </>
+        
+
     )
 }
 
